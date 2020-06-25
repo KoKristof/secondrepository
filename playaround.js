518481d0f8d0e869b64f0cde67ba7251d78a17c7
Kokristof <kovacskristofattila@gmail.com>
2020-06-25 19:52:18 +0200

    new

--- playaround.js
+++ playaround.js
@@ -1,7 +1,5 @@
 $("#first").text("Orange");
 $(".second").css("border-color", "yellow");
-let color  = $('.second').css('background-color',"purple") ; let number=10; let word=cool;
+let color  = $('#second').css('background-color',"purple") ; let number=10; let word=cool;
 
-if (100<number) {     
-    $(".second")
-}
+
